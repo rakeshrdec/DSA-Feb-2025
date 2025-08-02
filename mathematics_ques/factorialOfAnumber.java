77bd7ae4c242c2c4733dc9f08f4cdcdff59561e9
Rakesh Kumar Mishra <rakesh.mishra@instape.com>
2025-08-03 02:00:16 +0530

    updated 0 factorial

--- mathematics_ques/factorialOfAnumber.java
+++ mathematics_ques/factorialOfAnumber.java
@@ -5,9 +5,14 @@ public class factorialOfAnumber {
         System.out.println("Factoril is :"+x.factorial(15));
     }
     private int factorial(int n){
-        if(n<0 || n == 0)
+        if(n<0)
         {
             return -1;
+        }
+
+        if (n==0)
+        {
+            return 1;
         }
         int result = 1;
         while (n>1) {
@@ -16,6 +21,6 @@ public class factorialOfAnumber {
         }
         return result;
         // TIME Complexity : O(n)
-        // SPace complexity : O(n)
+        // SPace complexity : O(1)
     }
 }

577305529cd15e8c3d5b1c6254e65388f63c96f4
Rakesh Kumar Mishra <rakesh.mishra@instape.com>
2025-08-03 02:25:31 +0530

    added trailing zero count in a factorial method in java

---
+++ mathematics_ques/trailingZeros.java
@@ -0,0 +1,38 @@
+import java.math.BigInteger;
+
+public class trailingZeros {
+
+    public static BigInteger factorial(int n){
+        if(n<0)
+        {
+            return BigInteger.valueOf(-1);
+        }
+
+        if (n==0)
+        {
+            return BigInteger.valueOf(1);
+        }
+        BigInteger result =BigInteger.valueOf(1);
+        while (n > 1) {
+            result = result.multiply(BigInteger.valueOf(n));
+            n--;
+        }
+        return result;
+        // TIME Complexity : O(n)
+        // SPace complexity : O(1)
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Har Har Mahade !");
+        // trailingZeros t = new trailingZeros();
+        int num = 30;
+        // trailing zeros in factorial of goiven num 
+        int trailingZeroCOunt = 0;
+        for(int i = 5; i <= num; i *= 5)
+        {
+            trailingZeroCOunt = trailingZeroCOunt + num / i;
+        }
+        System.out.println("printing factorial of given num "+factorial(num));
+        System.out.println("trailing zero count of factorial of given number is :"+trailingZeroCOunt);
+    }
+}

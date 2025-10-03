c5743d6823b840d21b67f1c325088ab22d265330
Rakesh Mishra <rakesh.mishra@instape.com>
2025-10-04 01:40:39 +0530

    Queue revision and tree BFS Impl.. going on

--- tree/TreeBfs.java
+++ tree/TreeBfs.java
@@ -1,3 +1,49 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+
+}
+class QueueWithLinkedList {
+    Node front, rear;
+
+    QueueWithLinkedList()
+    {
+        front = rear = null;
+    }
+
+    public void Enqueue(int data)
+    {
+        Node node = new Node(data);
+        if(front==null || rear == null)
+        {
+            rear = node;
+            front = node;
+            front.next = rear;
+            return;
+        }
+        rear.next = node;
+        rear = node;
+    }
+
+    public void peekQueue()
+    {
+        System.out.print("printing the peek element of queue : => ");
+        System.out.println(front.data);
+    }
+
+    public void  Dequeue()
+    {
+        System.out.println("Deleting first element from the queue "+front.data);
+        front = front.next;
+    }
+}
+
 class TreeNode {
     int root;
     TreeNode Left, right;
@@ -28,53 +74,9 @@ class TreeNode {
 
     }
 
-    public void InOrderPrint()
-    {
-        // InOrder, (Left Root Right) 
-        if(this.Left != null)
-        {
-         this.Left.InOrderPrint();
-        } 
-        System.out.print(" "+this.root);
-        if (this.right != null) {
-            // System.out.print(" "+this.right);
-            this.right.InOrderPrint();
-        }
-        
-    }
-
-    public void PreOrderPrint()
-    {
-        // Preorder (Root → L → R)
-        System.out.print(" "+this.root);
-        if(this.Left != null)
-        {
-         this.Left.PreOrderPrint();
-        } 
-        if (this.right != null) {
-            // System.out.print(" "+this.right);
-            this.right.PreOrderPrint();
-        }
-        
-    }
-
-    public void PostOrderPrint()
-    {
-        // Postorder (L → R → Root)
-        if(this.Left != null)
-        {
-         this.Left.PostOrderPrint();
-        } 
-        if (this.right != null) {
-            // System.out.print(" "+this.right);
-            this.right.PostOrderPrint();
-        }
-         System.out.print(" "+this.root);
-        
-    }
-
 }
-public class TreeDfs {
+
+public class TreeBfs {
     // DFS (Depth FIrst Search/Traversal)
     // InOrder, (Left Root Right) 
     // PreOrder (Root Left Right)
@@ -108,11 +110,21 @@ public class TreeDfs {
     t10.right = t11;
 
     t1.printCurrentTreeNode();
-    System.out.println("IndOder Printing =>");
-    t1.InOrderPrint();
-    System.out.println(" \n Pre Order Printing");
-    t1.PreOrderPrint();
-    System.out.println(" \n Post Order Printing");
-    t1.PostOrderPrint();
+    System.out.println("Bread First Search , Level Order Printing =>");
+    // t1.LevelOrderPrinting();    
+        System.out.println("HK ... ! ..Coding mode truend on.. ");
+        QueueWithLinkedList q = new QueueWithLinkedList();
+        q.Enqueue(10);
+        q.Enqueue(20);
+        q.Enqueue(30);
+        q.peekQueue();
+        q.Dequeue();
+        q.Dequeue();
+        q.peekQueue();
+        q.Enqueue(10);
+        q.Enqueue(20);
+        q.Dequeue();
+        q.peekQueue();
+    
     }
 }
